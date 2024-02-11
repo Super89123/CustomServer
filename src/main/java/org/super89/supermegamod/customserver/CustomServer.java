08a8a3db7d89f94f476f273eee3cb5abcab1ff25
Super89123 <tedswa@mail.ru>
2024-02-11 21:32:20 +0400

    123

---
+++ src/main/java/org/super89/supermegamod/customserver/CustomServer.java
@@ -0,0 +1,17 @@
+package org.super89.supermegamod.customserver;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomServer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

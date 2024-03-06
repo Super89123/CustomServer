67d01800f5c98f323e2213768e4c7e9019858a1d
Nikita <nik.kiselev.2010@inbox.ru>
2024-03-06 23:50:33 +0400

    Merge remote-tracking branch 'origin/master'

--- src/main/java/org/super89/supermegamod/customserver/CustomSword.java
+++ src/main/java/org/super89/supermegamod/customserver/CustomSword.java
@@ -1,21 +1,18 @@
 package org.super89.supermegamod.customserver;
 
 import org.bukkit.Material;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.java.JavaPlugin;
 
 public class CustomSword implements Listener {
 
-    private final JavaPlugin plugin;
+    private CustomServer plugin;
 
-    public CustomSword(JavaPlugin plugin) {
+    public CustomSword(CustomServer plugin) {
         this.plugin = plugin;
-        plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
     @EventHandler
---
+++ src/main/java/org/super89/supermegamod/customserver/Madness.java
@@ -0,0 +1,7 @@
+package org.super89.supermegamod.customserver;
+
+import org.bukkit.event.Listener;
+
+public class Madness implements Listener {
+
+}

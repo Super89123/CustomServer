cbf1030eb38d9990504f6f9d20839eb4161e6551
Nikita <nik.kiselev.2010@inbox.ru>
2024-03-09 18:16:24 +0400

    Merge remote-tracking branch 'origin/master'

--- src/main/java/org/super89/supermegamod/customserver/Darkness.java
+++ src/main/java/org/super89/supermegamod/customserver/Darkness.java
@@ -1,6 +1,27 @@
 package org.super89.supermegamod.customserver;
 
-public class Darkness extends Event{
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.*;
+import org.bukkit.event.block.BlockDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+public final class Darkness implements Listener{
+
+    private CustomServer plugin;
+
+    public Darkness(CustomServer plugin){this.plugin=plugin;}
+    @EventHandler
+    public void onMove(PlayerMoveEvent e) {
+        Player player = e.getPlayer();
+        while (true){
+            if (player.getLocation().getBlock().getState().getLightLevel() < 6) {
+                player.setHealth(player.getHealth() - 2d);
+            }
+        }
+    }
 
 
 }
